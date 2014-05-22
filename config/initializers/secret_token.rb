87ecabd3a7184b51b97d9fa31893fbe89a7696ca
Amos Stone <amos@amosarts.com>
2014-05-21 20:03:35 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dfine::Application.config.secret_key_base = '33845fcb620a98a7fdee85dbc825f06e678dc622ac6839f6af4520ec6199eeeee8ead5f0d5d220013b5051f1e4a345ab9af339dcfef13a793b9a32d9d232e5d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dfine::Application.config.session_store :cookie_store, key: '_dfine_session'
